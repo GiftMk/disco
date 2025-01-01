16f7651748f3f917f058c8f935beb9ad8cc0cd78
Gift Mkwara <gift.mkwara@gmail.com>
2025-01-02 12:47:11 +1300

    implemented a basic temp file cleanup

--- packages/backend/src/lib/tempFile.ts
+++ packages/backend/src/lib/tempFile.ts
@@ -1,6 +1,23 @@
 import path from 'node:path'
 import os from 'node:os'
+import fs from 'node:fs'
+import nodeCleanup from 'node-cleanup'
+
+const filePaths: string[] = []
 
 export const tempFile = (filename: string): string => {
-	return path.join(os.tmpdir(), `${filename}`)
+	const filePath = path.join(os.tmpdir(), `${filename}`)
+	filePaths.push(filePath)
+
+	return filePath
 }
+
+export const cleanupTempFiles = () => {
+	for (const filePath of filePaths) {
+		if (fs.existsSync(filePath)) {
+			fs.rmSync(filePath)
+		}
+	}
+}
+
+nodeCleanup(cleanupTempFiles)
